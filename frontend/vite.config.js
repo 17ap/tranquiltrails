4f7b2255e83a2423cd37e1fca078fdf23ce9a52a
Алина Пяткевич <alinapatkevic@MacBook-Air-Alina.local>
2025-06-14 03:39:08 +0300

    preview config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   publicDir: 'public',
   plugins: [react()],
+  preview: {
+    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Your backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
